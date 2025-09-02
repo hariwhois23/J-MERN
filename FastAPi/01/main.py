ae05fed3e03de7013f0ed4842e4ecb32b4f8fcbe
hariwhois23 <shariprasath.in@gmail.com>
2025-09-02 23:28:22 +0530

    http GET

--- FastAPi/01/main.py
+++ FastAPi/01/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
-app = FastAPI()
+app = FastAPI() #app instance
 
-@app.get("/")
+@app.get("/") #Decorator that uses app instance 
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Welcome to my API"}
+
+
+@app.get("/post") #/post is the directory visit server/post
+def get_post():
+    return {"data" : "The List of posts"}
+
+
+@app.post("/create.post")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"Message": "created post"}
