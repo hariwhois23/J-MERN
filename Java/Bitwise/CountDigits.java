085ce23cefa4d7c10d3f24ddcd5a766871c8150d
hariwhois23 <shariprasath.in@gmail.com>
2025-09-30 11:57:52 +0530

    BIT manipulation

--- Java/Bitwise/CountDigits.java
+++ Java/Bitwise/CountDigits.java
@@ -1,10 +1,17 @@
 public class CountDigits {
     public static void main(String[] args) {
 
-        int num = 6;
-        System.out.println(Digits(num));
+        int num = 256;
+        System.out.println(Digits1(num,2));
 
     }
+//This works for all the bases 
+    static int Digits1(int num, int base){
+        int count = (int)(Math.log(num)/Math.log(base)) +1;
+        return count;
+
+    }
+
 
     static int Digits(int num) {
         int count = 0;
